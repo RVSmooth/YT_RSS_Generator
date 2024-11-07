d42d52c0e736739f3a33a1f8f4dfb7455fc83b99
RVSmooth <itzsubhashis@proton.me>
2024-11-07 19:39:17 +0530

    feat: Support multiple inputs for urls and handles

--- YRG.sh
+++ YRG.sh
@@ -10,13 +10,10 @@
 # example usage: ./YRG.sh https://www.youtube.com/@handle
 #
 
-url="$( echo $1 | awk -F '@' '{print "@"$2}' | awk -F '/' '{print "https://www.youtube.com/"$1}')"
-
 filtercmd="sed -n 's/.*\(https:\/\/www\.youtube\.com\/channel\/[a-zA-Z0-9_-]\{24\}\).*/\1/p' | sort | uniq"
 
 # Define the filter function
 function filter(){
-  local url="$1"
   if [[ -z "$url" ]]; then
     echo "Usage: filter <url>"
     return 1
@@ -24,10 +21,22 @@ function filter(){
   curl -s "$url" | eval "$filtercmd"
 }
 
+
+function filter_it(){
+
 filtered_output=$(filter "$url")
-echo -e "filtered_output: $filtered_output"
 channel_id="${filtered_output#*channel/}"
 converted_url="https://www.youtube.com/feeds/videos.xml?channel_id=$channel_id"
+
 echo -e "Link with id: $filtered_output"
 echo -e "RSS URL: $converted_url"
+echo
 
+}
+
+for handles in "$@"; do
+	channel_name="$( echo $handles | awk -F '@' '{print $2}')"
+	url="$( echo $handles | awk -F '@' '{print "@"$2}' | awk -F '/' '{print "https://www.youtube.com/"$1}')"
+	echo -e "Channel Name: $channel_name"
+	filter_it
+done
