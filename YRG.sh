9dfef03c1c4e0de4623f4de9b885b09adcc3a64e
RVSmooth <itzsubhashis@proton.me>
2024-11-07 19:57:59 +0530

    feat: Produce colored output

--- YRG.sh
+++ YRG.sh
@@ -10,6 +10,31 @@
 # example usage: ./YRG.sh https://www.youtube.com/@handle
 #
 
+#Some colors
+RESET='\033[0m'
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[0;33m'
+BLUE='\033[0;34m'
+
+# Color Functions
+
+ERED() {
+    echo -e "${RED}$@${RESET}"
+}
+
+EGREEN() {
+    echo -e "${GREEN}$@${RESET}"
+}
+
+EYELL() {
+    echo -e "${YELLOW}$@${RESET}"
+}
+
+EBLUE() {
+    echo -e "${BLUE}$@${RESET}"
+}
+
 filtercmd="sed -n 's/.*\(https:\/\/www\.youtube\.com\/channel\/[a-zA-Z0-9_-]\{24\}\).*/\1/p' | sort | uniq"
 
 # Define the filter function
@@ -28,8 +53,10 @@ filtered_output=$(filter "$url")
 channel_id="${filtered_output#*channel/}"
 converted_url="https://www.youtube.com/feeds/videos.xml?channel_id=$channel_id"
 
-echo -e "Link with id: $filtered_output"
-echo -e "RSS URL: $converted_url"
+EGREEN URL WITH ID:
+echo -e $filtered_output
+EBLUE RSS URL: 
+echo -e $converted_url
 echo
 
 }
@@ -37,6 +64,8 @@ echo
 for handles in "$@"; do
 	channel_name="$( echo $handles | awk -F '@' '{print $2}')"
 	url="$( echo $handles | awk -F '@' '{print "@"$2}' | awk -F '/' '{print "https://www.youtube.com/"$1}')"
-	echo -e "Channel Name: $channel_name"
+	EYELL CHANNEL: $channel_name
 	filter_it
 done
+
+
